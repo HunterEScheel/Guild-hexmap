20af98acf4e930c0a99a4819d65dca29d91b7065
HunterEScheel <hescheel@scheels.com>
2026-05-19 08:44:53 -0500

    fixing build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useMemo } from "react";
+import { useState, useCallback } from "react";
 import { HexGrid } from "./components/HexGrid";
 import { SidePanel } from "./components/SidePanel";
 import { AdminToolbar } from "./components/AdminToolbar";
